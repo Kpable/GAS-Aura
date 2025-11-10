7c11792fc17baf830c914f80a6b2fe8e5f654b4b
lestherreynoso <lhreynos@gmail.com>
2025-11-10 12:08:19 -0800

    Level Up Niagara Component

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -7,6 +7,10 @@
 #include "Interaction/PlayerInterface.h"
 #include "AuraCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+class UNiagaraComponent;
+
 /**
  * 
  */
@@ -37,7 +41,20 @@ public:
 	virtual int32 GetPlayerLevel_Implementation() override;
 	/** End Combat Interface */
 
-protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UNiagaraComponent> LevelUpNiagaraComponent;
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UCameraComponent> TopDownCameraComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USpringArmComponent> CameraBoom;
+	
 	virtual void InitAbilityActorInfo() override;
+
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastLevelUpParticles() const;
 	
 };
